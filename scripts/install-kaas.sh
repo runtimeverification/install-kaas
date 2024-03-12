052bb9b77e05a51f117baa9319c4401913bd6e5f
devops <devops@runtimeverification.com>
2024-03-11 22:57:33 -0600

    Action update and script naming

--- scripts/install-kaas.sh
+++ scripts/install-kaas.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-git clone git@github.com:runtimeverification/kaas.git "$@"
-cd kaas/kaas-cli
+if [ ! -d "kaas" ]; then
+  git clone https://github.com/runtimeverification/kaas.git\
+else
+  push kaas
+  git fetch && git pull
+  popd > /dev/null
+fi
+pushd kaas/kaas-cli
 make build
 pip install dist/*.whl
 poetry install
-poetry shell
-kaas-cli hello
+# poetry run kaas-cli hello
+poetry shell 
+popd > /dev/null
